ecd167bc77c0739fda4a97a22ec0c2ee8adfbc99
238SAMIxD <samijedrzejewski@gmail.com>
2022-02-08 21:43:20 +0100

    styles and scripts

---
+++ js/language.js
@@ -0,0 +1,36 @@
+ if( localStorage.getItem("language") === null ) localStorage.setItem("language", "pl");
+
+ fetch('js/translations.json')
+ .then(response => {
+   return response.json();
+})
+.then(json => {
+   return json[ localStorage.getItem("language") ];
+})
+.then(translations => {
+   document.querySelectorAll("[data-text]").forEach(element => {
+      let t = translations;
+
+      if( element.dataset.grandparent ) {
+         t = translations[ element.dataset.grandparent ];
+      }
+
+      if( element.dataset.parent ) {
+         t = translations[ element.dataset.parent ];
+      }
+
+      let x = "";
+
+      if( element.innerHTML.indexOf("</i>") > 0  ){
+         x = element.innerHTML.slice( 0, element.innerHTML.indexOf("</i>") + 4 ) + ' ';
+      }
+
+      element.innerHTML = x + t[ element.dataset.text ];
+   })
+});
+ 
+ function changeLanguage(lang) {
+    localStorage.setItem("language", lang);
+ }
+
+ 
