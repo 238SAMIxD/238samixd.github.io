66a81d490ccad143dff4921d3abb8cfcb833811a
238SAMIxD <samijedrzejewski@gmail.com>
2022-02-09 19:33:43 +0100

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,36 +1,38 @@
 if( localStorage.getItem("language") === null ) localStorage.setItem("language", "pl");
 
-fetch('js/translations.json')
-.then(response => {
-   return response.json();
-})
-.then(json => {
-   return json[ localStorage.getItem("language") ];
-})
-.then(translations => {
-   document.querySelectorAll("[data-text]").forEach(element => {
-	let t = translations;
-
-	if( element.dataset.grandparent ) {
-		t = t[ element.dataset.grandparent ];
-	}
-
-	if( element.dataset.parent ) {
-		t = t[ element.dataset.parent ];
-	}
-
-	let x = "";
-
-	if( element.innerHTML.indexOf("</i>") > 0  ){
-		x = element.innerHTML.slice( 0, element.innerHTML.indexOf("</i>") + 4 ) + ' ';
-	}
-
-	element.innerHTML = x + t[ element.dataset.text ];
-   })
-});
+changeLanguage( localStorage.getItem("language") );
  
 function changeLanguage(lang) {
     localStorage.setItem("language", lang);
+	
+	fetch('js/translations.json')
+	.then(response => {
+	   return response.json();
+	})
+	.then(json => {
+	   return json[ localStorage.getItem("language") ];
+	})
+	.then(translations => {
+	   document.querySelectorAll("[data-text]").forEach(element => {
+		let t = translations;
+
+		if( element.dataset.grandparent ) {
+			t = t[ element.dataset.grandparent ];
+		}
+
+		if( element.dataset.parent ) {
+			t = t[ element.dataset.parent ];
+		}
+
+		let x = "";
+
+		if( element.innerHTML.indexOf("</i>") > 0  ){
+			x = element.innerHTML.slice( 0, element.innerHTML.indexOf("</i>") + 4 ) + ' ';
+		}
+
+		element.innerHTML = x + t[ element.dataset.text ];
+	   })
+	});
  }
 
  
